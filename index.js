cc6a7fb376abcc43076adbae243cc168795f61d2
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-11-27 16:52:08 +0530

    17

--- index.js
+++ index.js
@@ -21,15 +21,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+import dbConnect from './src/config/mongodb.js';
+dbConnect();
+
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
 
 // Routes
 app.use('/api/auth', authRoutes);
