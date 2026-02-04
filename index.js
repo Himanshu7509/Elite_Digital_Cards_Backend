7c17e33d1abf33a9c5639d21091ea9d4399122e2
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2026-02-04 15:10:50 +0530

    28

--- index.js
+++ index.js
@@ -30,9 +30,35 @@ const PORT = process.env.PORT || 3000;
 
 import dbConnect from './src/config/mongodb.js';
 dbConnect();
+app.set('trust proxy', 1);
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'http://localhost:5174',
+  'http://127.0.0.1:5174',
+  'https://www.elitedigitalcards.com',
+  'https://elitedigitalcards.com',
+  'https://www.elitedigitalcards.in',
+  'https://elitedigitalcards.in',
+  'https://elite-cards-admin-panel.vercel.app'
+];
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
